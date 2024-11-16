272283ab1b9fb85d4bfc25ee8c04a78602c8ed72
Baxodir <baxodir-dev>
2024-11-16 17:28:28 +0500

    bu birinchi sayt

---
+++ main.js
@@ -0,0 +1,8 @@
+let num1 = prompt("ismingizni kiriting")
+let num2 = prompt("yoshingizni kiriting")
+if (num2 > 10) {
+alert("paladium saytiga xush kelibsiz" + " " + num1)
+}
+else{
+    alert("hali saytga kirishga yoshlik qilasan" + " " + num1)
+}
